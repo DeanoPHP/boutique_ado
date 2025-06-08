472c1e4ee3d5f35f5a3f13cf1ef60d7c19f0c867
DeanoLark <deanolark@gmail.com>
2025-06-08 13:58:11 +0100

    added shappong bag app, urls and template

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'home',
     'products',
+    'bag',
 
     # Optional -- requires install using `django-allauth[socialaccount]`.
     'allauth.socialaccount',
