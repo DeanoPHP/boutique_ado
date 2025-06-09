a53b68f45d78ddbf7b3a025c8a86bda934ccbed5
DeanoLark <deanolark@gmail.com>
2025-06-09 14:53:45 +0100

    finished product sizes logic

--- products/admin.py
+++ products/admin.py
@@ -4,21 +4,20 @@ from .models import Product, Category
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'sku', 
-        'name',
-        'category',
-        'price',
-        'rating',
-        'image',
+        "sku",
+        "name",
+        "category",
+        "price",
+        "rating",
+        "image",
     )
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
-        'friendly_name',
-        'name',
+        "friendly_name",
+        "name",
     )
-
 
 
 admin.site.register(Product, ProductAdmin)
