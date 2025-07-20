c9e183b7b421d2dd3e88a26ec35625431c68950b
DeanoLark <deanolark@gmail.com>
2025-07-20 18:41:08 +0100

    enabled edit product

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="home")]
+urlpatterns = [
+    path("", views.index, name="home"),
+    path("debug-request/", views.debug_request_view, name="debug-request"),
+]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.models import AnonymousUser
 
 # Create your views here.
 
@@ -6,3 +8,18 @@ from django.shortcuts import render
 def index(request):
     """A view to return the index page"""
     return render(request, "home/index.html")
+
+
+def debug_request_view(request):
+    info = {
+        "method": request.method,
+        "user": str(request.user),
+        "is_authenticated": request.user.is_authenticated,
+        "session": dict(request.session.items()),
+        "GET": dict(request.GET),
+        "POST": dict(request.POST),
+        "FILES": {k: v.name for k, v in request.FILES.items()},
+        "headers": dict(request.headers),
+        "cookies": request.COOKIES,
+    }
+    return JsonResponse(info, json_dumps_params={"indent": 2})
